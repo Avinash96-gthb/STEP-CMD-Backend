74b2ab1f5f3bb07815b8a451fa2ea7bcb08a0a70
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-06-27 12:15:10 +0530

    chore: added cors

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import { connectDB } from './config/database';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 import { seedIfNeeded } from './scripts/seed';
+import cors from './utils/cors';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
